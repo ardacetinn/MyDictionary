ddc81d6fb97bea3ca0c559c758a1868414a98de4
ardac <ardacetinn2@gmail.com>
2023-02-03 14:49:07 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<T, V>
+    {
+        T[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new V[0];
+        }
+
+        public void Add(T key, V value)
+        {
+            T[] tempArrayK = keys;
+            V[] tempArrayV = values;
+
+            keys = new T[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArrayK.Length; i++)
+            {
+                keys[i] = tempArrayK[i];
+                values[i] = tempArrayV[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine($"Key: {keys[i]}" + $" Value: {values[i]}");
+            }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Dictionary<int, string> cities = new Dictionary<int, string>();
+            cities.Add(01, "Adana");
+            cities.Add(02, "Ankara");
+            Console.WriteLine(cities.Count);
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            MyDictionary<int, string> cities2 = new MyDictionary<int, string>();
+            cities2.Add(01, "Adana");
+            cities2.Add(02, "Ankara");
+            cities2.Add(13, "Bitlis");
+            cities2.Add(33, "Mersin");
+            cities2.Add(34, "İstanbul");
+            Console.WriteLine(cities2.Count);
+
+            cities2.List();
+
+        }
+    }
+}
